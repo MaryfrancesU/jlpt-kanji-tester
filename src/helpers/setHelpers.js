4ef4e8e911d35298f19d8522194fbb2e45de5dfa
MaryfrancesU <55061982+MaryfrancesU@users.noreply.github.com>
2025-08-21 21:11:58 -0500

    update set Helpers

--- src/helpers/setHelpers.js
+++ src/helpers/setHelpers.js
@@ -1,14 +1,33 @@
-export function getStartIndex(setNumber, quizLength) {
-    return (quizLength * setNumber) - quizLength;
+import { getKanjiInfo } from "@/api/kanji";
+
+function getStartIndex(setNumber, quizLength) {
+    return quizLength * setNumber - quizLength;
 }
 
-export function getEndIndex(setNumber, quizLength) {
-    return (quizLength * setNumber);
+function getEndIndex(setNumber, quizLength) {
+    return quizLength * setNumber;
 }
 
 export function getStartAndEndIndex(setNumber, quizLength) {
     return {
         start: getStartIndex(setNumber, quizLength),
-        end: getEndIndex(setNumber, quizLength)
+        end: getEndIndex(setNumber, quizLength),
     };
+}
+
+export async function getKanjiForSet(startIndex, endIndex, levelKanji) {
+    const setKanji = levelKanji.slice(startIndex, endIndex);
+
+    const setKanjiWithInfo = await Promise.all(
+        setKanji.map(async (kanji) => {
+            const info = await getKanjiInfo(kanji);
+            return {
+                kanji,
+                meanings: info.meanings || [],
+                kunyomi: info.kun_readings || [],
+                onyomi: info.on_readings || [],
+            };
+        })
+    );
+    return setKanjiWithInfo;
 }
